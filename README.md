e23eb7e0a200af6e7a51acf6baff5c544f8dc747
NegatioN <joakimrishaug@gmail.com>
2020-08-16 01:30:24 +0200

    persist a server url

---
+++ README.md
@@ -0,0 +1,4 @@
+# firefox-link-extension
+
+A simple extension which executes a get request with the currently selected
+tab url to a server url of your choice.
--- popup/tabinfo.js
+++ popup/tabinfo.js
@@ -1,8 +1,17 @@
 function setPageInfo() {
     browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
-        let tabsList = document.getElementById('pageurl');
-        tabsList.value = tabs[0].url;
+        let pageurl = document.getElementById('pageurl');
+        let serverurl = document.getElementById('serverurl');
+        pageurl.value = tabs[0].url;
+        browser.storage.local.get('server').then(item => {
+            serverurl.value = item.server.value;
+        })
     })
 }
 
 document.addEventListener("DOMContentLoaded", setPageInfo);
+
+document.getElementById('serverurl').addEventListener('change', function (evt) {
+    let server = {name: 'server', value: this.value}
+    browser.storage.local.set({server}).then(res => console.log("Updated server value"))
+});

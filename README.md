76b9ff0d6713b7dbab363d84e1473a5aec457616
NegatioN <joakimrishaug@gmail.com>
2020-08-16 01:58:11 +0200

    Actually execute a get request to the listed url

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 A simple extension which executes a get request with the currently selected
 tab url to a server url of your choice.
+
+### TODO
+1. Add open shortcut? https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/user_interface/Popups
+2. Animate success/Failure of request by adding some sort of icon afterwards.______
--- popup/tabinfo.js
+++ popup/tabinfo.js
@@ -15,3 +15,24 @@ document.getElementById('serverurl').addEventListener('change', function (evt) {
     let server = {name: 'server', value: this.value}
     browser.storage.local.set({server}).then(res => console.log("Updated server value"))
 });
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+document.getElementById("send").addEventListener("click", (e) => {
+    let serverurl = document.getElementById('serverurl');
+    let pageurl = document.getElementById('pageurl');
+
+    let url = encodeURI(`${serverurl.value}${pageurl.value}`);
+    fetch(url).then(response => response.json())
+        .then(data => {
+            pageurl.value = "Success";
+        })
+        .catch(err => {
+            pageurl.value = "Fail";
+        })
+        .then(r => {
+            sleep(1500).then(rr => window.close());
+        });
+});
